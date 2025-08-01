c9521db31c395756bb78469ba6bab282d78ffd51
MarkIrahCarey <markiragalang1024@gmail.com>
2025-08-01 15:14:54 +1000

    fixed movement logic, working on capture logic

--- core/BoardManager.java
+++ core/BoardManager.java
@@ -45,7 +45,7 @@ public class BoardManager {
 
         // add movement logic to that square
         addBoardClickListeners();
-        
+
         // display window
         gameWindow.setVisible(true);
     }
@@ -62,6 +62,7 @@ public class BoardManager {
                         // If this square is highlighted and there's a selected piece, move to it
                         if (selectedPiece != null && gui.isHighlighted(r, c)) {
                             movePiece(selectedPiece, r, c);
+                            
                             gui.clearHighlights();
                             selectedPiece = null;
                         }
@@ -151,28 +152,35 @@ public class BoardManager {
             case "Pawn":
                 label.addMouseListener(new MouseAdapter() {
                     public void mouseClicked(MouseEvent e) {
-                        selectedPiece = piece;
+                        if (deselectIfAlreadySelected(piece)){return;}
+                        else {
+                            selectedPiece = piece;
 
-                        int row = piece.getRow();
-                        int col = piece.getCol();
-                        int dir = piece.getColor().equals("White") ? -1 : 1;
-                        
-                        gui.clearHighlights();
+                            int row = piece.getRow();
+                            int col = piece.getCol();
 
-                        // Forward one square
-                        if (isValid(row + dir, col) && gui.getSquareText(row + dir, col).equals("")) {
-                            gui.highlightSquare(row + dir, col, Color.GREEN);
+                            int dir = piece.getColor().equals("White") ? -1 : 1;
+                            
+                            gui.clearHighlights();
+                            
+                            // first highlight the piece itself
+                            gui.highlightSquare(row, col, Color.GRAY);
+
+                            // Forward one square
+                            if (isValid(row + dir, col) && gui.getSquareText(row + dir, col).equals("")) {
+                                gui.highlightSquare(row + dir, col, Color.GREEN);
+                            }
+
+                            // Forward two squares on first move
+                            if (!piece.hasMoved()
+                                    && isValid(row + dir, col) && gui.getSquareText(row + dir, col).equals("")
+                                    && isValid(row + 2 * dir, col) && gui.getSquareText(row + 2 * dir, col).equals("")) {
+                                gui.highlightSquare(row + 2 * dir, col, Color.GREEN);
+                            }
+                            
+                            // update position (implemented later, for now we add this)
+                            System.out.println("Pawn clicked at: " + row + "," + col);
                         }
-
-                        // Forward two squares on first move
-                        if (!piece.hasMoved()
-                                && isValid(row + dir, col) && gui.getSquareText(row + dir, col).equals("")
-                                && isValid(row + 2 * dir, col) && gui.getSquareText(row + 2 * dir, col).equals("")) {
-                            gui.highlightSquare(row + 2 * dir, col, Color.GREEN);
-                        }
-                        
-                        // update position (implemented later, for now we add this)
-                        System.out.println("Pawn clicked at: " + row + "," + col);
                     }
                 });
                 break;
@@ -180,46 +188,56 @@ public class BoardManager {
             case "Knight":
                 label.addMouseListener(new MouseAdapter() {
                     public void mouseClicked(MouseEvent e) {
-                        selectedPiece = piece;
+                        if (deselectIfAlreadySelected(piece)){return;}
+                        else {
+                            selectedPiece = piece;
 
-                        int row = piece.getRow();
-                        int col = piece.getCol();
+                            int row = piece.getRow();
+                            int col = piece.getCol();
 
-                        gui.clearHighlights();
-                        // for knight logic, its a 6 Ls around itself
-                        // easiest I can think of is that we go two spaces in North, East, South, and West, then look at the adjacent squares
-                        // we will just use sin and cosine to roate the L shape
-                        int[][] L_shapes = {{2, 1}, {1, 2}};
-                        
-                        for (int[] L : L_shapes){
-                            int init_row = L[0];
-                            int init_col = L[1];
+                            gui.clearHighlights();
 
-                            for (int degree = 0; degree < 360; degree += 90){
-                                double radians = Math.toRadians(degree);
+                            // first highlight the piece itself
+                            gui.highlightSquare(row, col, Color.GRAY);
 
-                                // get the rotation for row and column
-                                int rotatedRowOffset = (int) Math.round(init_row * Math.cos(radians) - init_col * Math.sin(radians));
-                                int rotatedColOffset = (int) Math.round(init_row * Math.sin(radians) + init_col * Math.cos(radians));
-                                
-                                // then apply that to the location of the knight
-                                int newRow = row + rotatedRowOffset;
-                                int newCol = col + rotatedColOffset;
-                                
-                                // then check for position is it is valid
-                                if (isValid(newRow, newCol) && gui.getSquareText(newRow, newCol).equals("")) {
-                                    gui.highlightSquare(newRow, newCol, Color.GREEN);
+                            // for knight logic, its a 6 Ls around itself
+                            // easiest I can think of is that we go two spaces in North, East, South, and West, then look at the adjacent squares
+                            // we will just use sin and cosine to roate the L shape
+                            int[][] L_shapes = {{2, 1}, {1, 2}};
+                            
+                            for (int[] L : L_shapes){
+                                int init_row = L[0];
+                                int init_col = L[1];
+
+                                for (int degree = 0; degree < 360; degree += 90){
+                                    double radians = Math.toRadians(degree);
+
+                                    // get the rotation for row and column
+                                    int rotatedRowOffset = (int) Math.round(init_row * Math.cos(radians) - init_col * Math.sin(radians));
+                                    int rotatedColOffset = (int) Math.round(init_row * Math.sin(radians) + init_col * Math.cos(radians));
+                                    
+                                    // then apply that to the location of the knight
+                                    int newRow = row + rotatedRowOffset;
+                                    int newCol = col + rotatedColOffset;
+                                    
+                                    // then check for position is it is valid
+                                    if (isValid(newRow, newCol)){
+                                        if (gui.getSquareText(newRow, newCol).equals("")) {
+                                            gui.highlightSquare(newRow, newCol, Color.GREEN);
+                                        } else {
+                                            checkEnemyPiece(piece, newRow, newCol);
+                                        }
+                                    } 
                                 }
                             }
+                            System.out.println("Knight clicked at: " + row + "," + col);
                         }
-                        System.out.println("Knight clicked at: " + row + "," + col);
                     }
                 });
                 break;
             case "Bishop":
                 label.addMouseListener(new MouseAdapter() {
                     public void mouseClicked(MouseEvent e) {
-                        selectedPiece = piece;
                         pieceLogicQueenRookBishop(piece);
                     }
                 });
@@ -227,7 +245,6 @@ public class BoardManager {
             case "Rook":
                 label.addMouseListener(new MouseAdapter() {
                     public void mouseClicked(MouseEvent e) {
-                        selectedPiece = piece;
                         pieceLogicQueenRookBishop(piece);
                     }
                 });
@@ -235,7 +252,6 @@ public class BoardManager {
             case "Queen":
                 label.addMouseListener(new MouseAdapter() {
                     public void mouseClicked(MouseEvent e) {
-                        selectedPiece = piece;
                         pieceLogicQueenRookBishop(piece);
                     }
                 });
@@ -244,26 +260,36 @@ public class BoardManager {
             case "King":
                 label.addMouseListener(new MouseAdapter() {
                     public void mouseClicked(MouseEvent e){
-                        selectedPiece = piece;
+                        if (deselectIfAlreadySelected(piece)){return;}
+                        else {
+                            selectedPiece = piece;
 
-                        int row = piece.getRow();
-                        int col = piece.getCol();
-                        gui.clearHighlights();
+                            int row = piece.getRow();
+                            int col = piece.getCol();
+                            gui.clearHighlights();
 
-                        // King is just a 1 square around
-                        int[][] directions = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+                            // highlight piece itself
+                            gui.highlightSquare(row, col, Color.GRAY);
 
-                        // then we loop through
-                        for (int[] dir : directions){
-                            int x = dir[0] + row;
-                            int y = dir[1] + col;
+                            // King is just a 1 square around
+                            int[][] directions = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
 
-                            if (isValid(x, y) &&
-                                gui.getSquareText(x, y).equals("")) {
-                                gui.highlightSquare(x, y, Color.GREEN);
+                            // then we loop through
+                            for (int[] dir : directions){
+                                int x = dir[0] + row;
+                                int y = dir[1] + col;
+
+                                if (isValid(x, y)){
+                                    if (gui.getSquareText(x, y).equals("")) {
+                                        gui.highlightSquare(x, y, Color.GREEN);
+                                    } else {
+                                        checkEnemyPiece(piece, x, y);
+                                    }
+                                }
+                                    
                             }
+                            System.out.println("King clicked at: " + row + "," + col);
                         }
-                        System.out.println("King clicked at: " + row + "," + col);
                     }
                 }); 
                 break;
@@ -274,19 +300,6 @@ public class BoardManager {
         System.out.println("Added Functionality to " + name);
     }
 
-    private void addMovementLogic(int row, int col){
-        if (selectedPiece != null) {
-            int destRow = row;
-            int destCol = col;
-
-            // Check if square is highlighted
-            if (gui.isHighlighted(destRow, destCol)) {
-                movePiece(selectedPiece, destRow, destCol);
-                gui.clearHighlights();
-                selectedPiece = null;
-            }
-        }
-    }
     private void movePiece(Piece piece, int newRow, int newCol){
         int oldRow = piece.getRow();
         int oldCol = piece.getCol();
@@ -332,49 +345,88 @@ public class BoardManager {
 
     // since the Queen is both the rook and bishop, I combined all of the logic together
     private void pieceLogicQueenRookBishop(Piece piece){
-        int row = piece.getRow();
-        int col = piece.getCol();
-        gui.clearHighlights();
+        if (deselectIfAlreadySelected(piece)){return;} 
+        else {
+            int row = piece.getRow();
+            int col = piece.getCol();
+            gui.clearHighlights();
 
-        String name = piece.getName();
+            selectedPiece = piece;
 
-        // then, queen is both bishop and rook, so we take dependent on name
-        int[][] directions = {
-            {-1, 0}, {1, 0}, {0, -1}, {0, 1},     // Rook directions
-            {-1, -1}, {-1, 1}, {1, -1}, {1, 1}    // Bishop directions
-        };
+            // highlight said piece
+            gui.highlightSquare(row, col, Color.GRAY);
 
-        int start = 0;
-        int end = directions.length;
+            String name = piece.getName();
 
-        switch (name){
-            case "Rook": end = 4;  // Only the first 4 directions
-            break;
+            // then, queen is both bishop and rook, so we take dependent on name
+            int[][] directions = {
+                {-1, 0}, {1, 0}, {0, -1}, {0, 1},     // Rook directions
+                {-1, -1}, {-1, 1}, {1, -1}, {1, 1}    // Bishop directions
+            };
 
-            case "Bishop": start = 4; // Only the last 4 directions
-            break;
+            int start = 0;
+            int end = directions.length;
 
-            case "Queen": // Use all 8 directions
-            break;
+            switch (name){
+                case "Rook": end = 4;  // Only the first 4 directions
+                break;
 
-            default:
-            return;
-        }
+                case "Bishop": start = 4; // Only the last 4 directions
+                break;
 
-        for (int i = start; i < end; i++){
-            // grab the directions
-            int x = directions[i][0];
-            int y = directions[i][1];
+                case "Queen": // Use all 8 directions
+                break;
 
-            // then get the distance for each square
-            int distance = 1;
-
-            while (isValid(row + x * distance, col + y * distance) &&
-                gui.getSquareText(row + x * distance, col + y * distance).equals("")) {
-                gui.highlightSquare(row + x * distance, col + y * distance, Color.GREEN);
-                distance++;
+                default:
+                return;
             }
+
+            for (int i = start; i < end; i++){
+                // grab the directions
+                int x = directions[i][0];
+                int y = directions[i][1];
+
+                // then get the distance for each square
+                int distance = 1;
+                
+                while (isValid(row + x * distance, col + y * distance)){
+                    if (gui.getSquareText(row + x * distance, col + y * distance).equals("")){
+                        gui.highlightSquare(row + x * distance, col + y * distance, Color.GREEN);
+                    } else {
+                        checkEnemyPiece(piece, row + x * distance, col + y * distance);
+                        break;
+                    } 
+                    
+                    distance++;
+                }
+            }
+            System.out.println(name + " clicked at: " + row + "," + col);
         }
-        System.out.println(name + " clicked at: " + row + "," + col);
     }
+
+    private void checkEnemyPiece(Piece piece, int row, int col){
+        // all logic about a piece capturing is handelled in the addFunctionality
+        // this method is created to reduce the clutter
+        
+        String color = piece.getColor();    
+
+        // we check if the square is a enemy tile by getting the text of that square
+        String color_init_enemy = gui.getSquareText(row, col).split(" ")[0];
+
+        // Then check if they are not the same color, if they are not, then we highlight it red
+        // to indicate possible capture
+        if (!color.equals(color_init_enemy)){
+            gui.highlightSquare(row, col, Color.RED);
+        }
+    }
+
+    private boolean deselectIfAlreadySelected(Piece piece) {
+        if (selectedPiece == piece) {
+            selectedPiece = null;
+            gui.clearHighlights();
+            return true;
+        }
+        return false;
+    }
+
 }
